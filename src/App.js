dde6aa143721f3e9805457f9d68966fb95d9b060
MatteoMatteoMatteo <s1910629019@students.fh-hagenberg.at>
2021-02-11 19:22:49 +0100

    netlify

--- src/App.js
+++ src/App.js
@@ -1,33 +1,78 @@
 import './App.css';
 import classNames from 'classnames';
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
+import * as Tone from "tone"
+import song1 from "./Assets/song1.mp3";
+import song2 from "./Assets/song2.mp3";
+import song3 from "./Assets/song3.mp3";
+import song4 from "./Assets/song4.mp3";
+import song5 from "./Assets/song5.mp3";
+import song6 from "./Assets/song6.mp3";
+import song7 from "./Assets/song7.mp3";
+import song8 from "./Assets/song8.mp3";
+import song9 from "./Assets/song9.mp3";
 
 function App() {
-  var liClasses = classNames(
-  
+  const liClasses = classNames(
     'button'
   );
 
+
+  const songArray = [
+    new Tone.Player({url:song1}).toMaster(),
+    new Tone.Player({url:song2}).toMaster(),
+    new Tone.Player({url:song3}).toMaster(),
+    new Tone.Player({url:song4}).toMaster(),
+    new Tone.Player({url:song5}).toMaster(),
+    new Tone.Player({url:song6}).toMaster(),
+    new Tone.Player({url:song7}).toMaster(),
+    new Tone.Player({url:song8}).toMaster(),
+    new Tone.Player({url:song9}).toMaster()]
+  
+  const [buffer, setBuffer] = useState(false);
   const [date, setDate] = useState();
   const [stretch, setStretch] = useState();
   const [letsGo, setLetsGo] = useState(false);
+  const [song, setSong] = useState(false);
+
+  useEffect(()=>{
+    Tone.Buffer.on("load", ()=> {
+      setBuffer(true)
+    })
+  },[])
 
 
   const handleAll = () => {
+
   setLetsGo(true);
-  handleClock()
+  // handleClock()
   handleStretch()
+  handleAudio()
+  } 
+
+  const handleAudio = () => {
+    setTimeout(()=>{var item = songArray[Math.floor(Math.random() * songArray.length)];
+      Tone.start()
+      var ohoh=false
+      setInterval(()=>{
+          if(new Date().getMinutes()==23 && item.state==="stopped" && !ohoh){
+            setSong(true)
+            item.start()
+            ohoh=true
+          }else if(item.state==="stopped" && ohoh){
+            setSong(false)
+            ohoh=false
+          }
+      },1000)},4000)
   }
 
   const handleStretch = () => {
-    var timeUntil = 60-new Date().getMinutes()
+    var timeUntil = 59-new Date().getMinutes()
     setStretch(()=>timeUntil)
-    
-    console.log("hey")
 
 
     setInterval(()=>{
-      var timeUntil = 60-new Date().getMinutes()
+      var timeUntil = 59-new Date().getMinutes()
       setStretch(()=>timeUntil)
     },1000)
 
@@ -52,20 +97,31 @@ function App() {
 
 
   return (
+    buffer?(    
     <div className="App">
-      <div className="item">TIME TO STRETCH</div>
-      <div className="item" style={{fontSize:"1.3rem", fontStyle:"italic", padding:"5px"}}>every full hour</div>
-    
-      {letsGo?(
-     <Fragment>
-       <div className="until"> <span style={{color:"#5cdb95"}}>{stretch}</span> min to next Stretch!</div>
-       {/* <div className="time">Time: {date}</div> */}
-      </Fragment>
-      ):<button onClick={()=>{handleAll()
-      }} className={liClasses}>START<br></br> ROUTINE</button>}
+    <div className="item">TIME TO STRETCH</div>
+    <div className="item" style={{fontSize:"1.3rem", fontStyle:"italic", padding:"5px"}}>every full hour</div>
+  
+    {letsGo?(
+
+    song?
+    <Fragment>
+    <div className="until" style={{marginBottom:"35px", marginTop:"50px"}}>Let's Stretch!</div>
+    <iframe src="https://giphy.com/embed/60wLzllpJn15e" width="430" height="310" style={{pointerEvents:"none"}} frameBorder="0"  allowFullScreen></iframe><p><a href="https://giphy.com/gifs/stretching-xhDniL1GXz43u"></a></p>
+    </Fragment>
+    :
+    <Fragment>
+    <div className="until"> <span style={{color:"#5cdb95"}}>{stretch}</span> min to next Stretch!</div>
+    {/* <div className="time">Time: {date}</div> */}
+    </Fragment>)
+
+    :
+    <button onClick={()=>{handleAll()
+    }} className={liClasses}>START<br></br> ROUTINE</button>}
 
 
-    </div>
+  </div>):(<div className="loading">SONGS FOR STRETCHING ARE LOADING...</div>)
+
   );
 }
 
