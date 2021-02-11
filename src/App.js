8995e3a9680acc3248940a04ab4b24048ec271de
MatteoMatteoMatteo <s1910629019@students.fh-hagenberg.at>
2021-02-11 16:32:59 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import classNames from 'classnames';
+import { Fragment, useState } from 'react';
 
 function App() {
+  var liClasses = classNames(
+  
+    'button'
+  );
+
+  const [date, setDate] = useState();
+  const [stretch, setStretch] = useState();
+  const [letsGo, setLetsGo] = useState(false);
+
+
+  const handleAll = () => {
+  setLetsGo(true);
+  handleClock()
+  handleStretch()
+  }
+
+  const handleStretch = () => {
+    var timeUntil = 60-new Date().getMinutes()
+    setStretch(()=>timeUntil)
+    
+    console.log("hey")
+
+
+    setInterval(()=>{
+      var timeUntil = 60-new Date().getMinutes()
+      setStretch(()=>timeUntil)
+    },1000)
+
+  }
+
+  const handleClock = () => {
+      var currentdate = new Date(); 
+      var datetime = 
+                      currentdate.getHours() + ":"  
+                      + (currentdate.getMinutes()<10?'0':'') + currentdate.getMinutes() + ":" 
+                      + currentdate.getSeconds();
+      setDate(()=>datetime)
+      setInterval(()=>{
+        var currentdate = new Date(); 
+        var datetime = 
+                        currentdate.getHours() + ":"  
+                        + (currentdate.getMinutes()<10?'0':'') + currentdate.getMinutes()  + ":" 
+                        + currentdate.getSeconds();
+        setDate(()=>datetime)
+      },1000)
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="item">TIME TO STRETCH</div>
+      <div className="item" style={{fontSize:"1.3rem", fontStyle:"italic", padding:"5px"}}>every full hour</div>
+    
+      {letsGo?(
+     <Fragment>
+       <div className="until"> <span style={{color:"#5cdb95"}}>{stretch}</span> min to next Stretch!</div>
+       {/* <div className="time">Time: {date}</div> */}
+      </Fragment>
+      ):<button onClick={()=>{handleAll()
+      }} className={liClasses}>START<br></br> ROUTINE</button>}
+
+
     </div>
   );
 }
